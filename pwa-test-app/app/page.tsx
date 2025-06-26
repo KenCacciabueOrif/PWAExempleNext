8f1ebe05b1560eac293eef9d55f49a8ef1955b93
Ken Cacciabue <ken.cacciabue@formation.orif.ch>
2025-06-26 09:36:17 +0200

    service worker

--- pwa-test-app/app/page.tsx
+++ pwa-test-app/app/page.tsx
@@ -1,8 +1,14 @@
 import Image from "next/image";
+import Head from 'next/head';
+import SWRegister from "./_components/SWRegister";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Head>
+        <link rel="icon" href="/apple-touch-icon.png" />
+      </Head>
+      <SWRegister/>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
